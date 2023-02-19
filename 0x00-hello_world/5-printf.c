9fe46e9e142e3ab620cf5ae9afa4120dbc3226ff
youssef twheed <yousseftwheed@gmail.com>
2023-02-19 02:24:15 -0800

    5-printf.c v.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -entry point of programe
+* Description : small program
+* Return: 0 (Success)
+*/
+int  main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
+
