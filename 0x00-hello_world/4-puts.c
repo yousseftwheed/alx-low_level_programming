864aefb1164a2b55896168bf4d47b9ebdfec6b9d
youssef twheed <yousseftwheed@gmail.com>
2023-02-19 02:10:37 -0800

    4-puts.c v.0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -entry point of programe
+* Description : small program
+* Return :' Always' 0 (Success)
+*/
+int  main(void)
+
+{
+char my_string[] = "\"Programming is like building a multilingual puzzle";
+puts(my_string);
+return (0);
+}
+
