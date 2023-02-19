cb7076ac4bea58e0cc79cd7c5812e85c4353920a
youssef twheed <yousseftwheed@gmail.com>
2023-02-19 06:36:19 -0800

    101-qoutesv.1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*main -entry point
+*Description: data type size
+*Return: 1 (success)
+*/
+int main(void)
+{
+char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int i;
+for (i = 0; i != EOF; i++)
+putchar(c);
+return (1);
+}
