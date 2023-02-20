6bbfed2e4b010044db1b8a9e4fac83310458e904
youssef twheed <yousseftwheed@gmail.com>
2023-02-20 06:08:58 -0800

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+*main -the entry point
+*Description: printing numbers
+*Return: Always 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (i < j)
+{
+putchar(i + 48);
+putchar(j + 48);
+if (i == 8)
+break;
+
+putchar(' ');
+putchar(',');
+}
+
+}
+
+}
+putchar('\n');
+return (0);
+}
