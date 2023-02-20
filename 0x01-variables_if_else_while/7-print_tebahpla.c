c7b0227e92661c54b0edaaa42e94fa0f9a67631d
youssef twheed <yousseftwheed@gmail.com>
2023-02-20 01:31:55 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main -entry point of programe
+* Description : small program
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+putchar('\n');
+return (0);
+}
