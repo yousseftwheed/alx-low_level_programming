c654eaa18afddd2b70b6a8b82859043a2f931dfc
youssef twheed <yousseftwheed@gmail.com>
2023-02-20 00:57:32 -0800

    6-print_numberz  v.0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+*main -the entry point
+*Description: printing numbers
+*Return: Always 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+	putchar(48 + i);
+putchar('\n');
+
+return (0);
+}
