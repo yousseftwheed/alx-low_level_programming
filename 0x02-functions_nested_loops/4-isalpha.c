c0c99b3dfe6e4f7574fccdc0635045c2790d998b
youssef twheed <yousseftwheed@gmail.com>
2023-02-22 15:55:33 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+include"main.h"
+
+/**
+*_isalpha -check lower case letters
+*Description: isalpha  task
+*@c: passed asscii number of the char
+*Return: 1 if letter & 0 is not letter
+*/
+
+int _isalpha(int c)
+{
+
+if (c >= 97 && c <= 122 || c <= 65 && c >= 90)
+return (1);
+else
+return (0);
+}
