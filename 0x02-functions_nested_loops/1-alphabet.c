62c016b34e967706b4966924db652f5902a5f6f4
youssef twheed <yousseftwheed@gmail.com>
2023-02-22 13:54:28 -0800

    print_alphabet v.0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+*print_alphabet -printing letters
+*Description: putchar task
+*/
+
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
