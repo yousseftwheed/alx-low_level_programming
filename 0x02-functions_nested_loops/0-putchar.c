0881d7956485526743c2f763a7f66134530bf7dc
youssef twheed <yousseftwheed@gmail.com>
2023-02-22 13:36:09 -0800

    putchar v.03

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 int i;
-char arr[] = "_putchar\n";
-for (i = 0; i < 10; i++)
+char arr[] = "_putchar";
+for (i = 0; i < 9; i++)
 _putchar(arr[i]);
-
+_putchar('\n');
 return (0);
 }
