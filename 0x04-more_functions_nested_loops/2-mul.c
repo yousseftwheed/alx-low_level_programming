8add0792611f48e757f25d15da20272234bab966
youssef twheed <yousseftwheed@gmail.com>
2023-02-23 15:20:28 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+*_islower -check lower case letters
+*Description: putchar task
+*@a:passed int number
+*@b:passed int number
+*Return: result of a*b
+*/
+
+int mul(int a, int b)
+{
+int c;
+int c = a * b;
+return (c);
+}
