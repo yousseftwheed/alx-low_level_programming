6d24942e898536302ad9837db93b5f2dd44652c6
youssef twheed <yousseftwheed@gmail.com>
2023-02-22 17:23:35 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+*_abs - to check for abs value
+*Description: task 6
+*@x: passed number to be checked
+*Return: x if +ve && x *-1 if -ve
+*/
+
+int _abs(int x)
+{
+if (x >= 0)
+return (x);
+else
+return (x * -1);
+}
