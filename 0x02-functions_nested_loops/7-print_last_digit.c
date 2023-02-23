171db66e635bc30346006e1430c09ea1512005c5
youssef twheed <yousseftwheed@gmail.com>
2023-02-22 18:03:13 -0800

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+*print_last_digit -recursive function
+*Description: task 7 last
+*@x: passed asscii number of the char
+*Return: last digit of number
+*/
+int print_last_digit(int x)
+{
+if (x / 10 == 0)
+{
+
+_putchar(x + 48);
+return (1);
+}
+print_last_digit(x / 10);
+return (1);
+
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+*main -entrypoint
+*Description: putchar task
+*Return: Always 0
+*/
+
+int main(void)
+{
+
+/*///print_alphabet();
+print_alphabet_x10();*/
+print_last_digit(8976);
+return (0);
+}
