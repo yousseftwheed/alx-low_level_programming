03cbfb1ac4b3089f6c671aa79ba9d2a84f4e93f9
youssef twheed <yousseftwheed@gmail.com>
2023-02-23 17:22:46 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+*print_line -prints _
+*Description:morefunction
+*@n: number of _ line consists of
+*/
+void print_line(int n)
+{
+if (n > 0)
+{
+while (n)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
