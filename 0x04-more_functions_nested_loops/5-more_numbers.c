64e911dc457fa53ed6ff8bda7edfab0e92f75359
youssef twheed <yousseftwheed@gmail.com>
2023-02-23 16:15:36 -0800

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+* more_numbers -the entry point
+*Description: printing numbers
+*/
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
