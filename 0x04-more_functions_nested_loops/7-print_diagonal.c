bbcdef21cc921af93ff638578f13edd7857c4bf2
youssef twheed <yousseftwheed@gmail.com>
2023-02-23 18:15:43 -0800

    void print_diagonal(int n);

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+*print_diagonal -print slash
+*Description: task 7
+*@n: number of slashes
+*/
+
+
+void print_diagonal(int n)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+if (n < 0)
+_putchar('\n');
+}
