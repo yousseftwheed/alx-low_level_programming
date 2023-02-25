2917a0a236df286531eadd5e9988007770775635
youssef twheed <yousseftwheed@gmail.com>
2023-02-24 21:20:37 -0800

    9-fizz_buzz v.4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else
-			if (i % 3 == 0 && i % 5 != 0)
+			if (i % 3 == 0)
 				printf("Fizz");
 			else
-				if (i % 5 == 0 && i % 3 != 0)
+				if (i % 5 == 0)
 					printf("Buzz");
 				else
 					printf("%d", i);
