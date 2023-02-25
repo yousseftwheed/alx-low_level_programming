9006fb72261c3ca6c9728abc26c218919f97b2a8
youssef twheed <yousseftwheed@gmail.com>
2023-02-25 04:05:10 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+*print_number -recursive_fun
+*@n :the number to be display
+*/
+
+void print_number(int n)
+{
+int x;
+x = n;
+if (n < 0)
+{
+x = -x;;
+_putchar('-');
+}
+if (x / 10)
+{	
+print_number(x /10);
+}
+
+_putchar('0' + x % 10);
+
+
+}
+
